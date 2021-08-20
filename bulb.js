4c376be48a7b21df98df9e04191fa9b9a5d42341
Abhi Singh <88607051+AbhiSingh58@users.noreply.github.com>
2021-08-20 12:15:17 +0530

    Create bulb.js

---
+++ bulb.js
@@ -0,0 +1,20 @@
+ const image = document.getElementById("img")
+        const onb = document.getElementById("on")
+        const offb = document.getElementById("off")
+        onb.addEventListener("click", () => {
+            if (image.src.match("of")) {
+                image.src = "pic_bulbon.gif";
+
+                console.log("hi")
+            }
+        })
+
+        offb.addEventListener("click", () => {
+            if (image.src.match("on")) {
+                image.src = "bulbof.gif"
+                console.log("hey")
+            }
+
+
+
+        })
